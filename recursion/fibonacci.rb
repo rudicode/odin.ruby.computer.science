cc3ec7ee699ef5ac6ad87087edae7d22aa0ff1f4
Rudi Luzar <rudi@rudicode.com>
2020-05-17 20:03:15 -0400

    fibonacci: completed

---
+++ recursion/fibonacci.rb
@@ -0,0 +1,27 @@
+
+
+def fibs(n)
+  return 0 if n == 0
+  return 1 if n <= 2
+  sum = sum_pre = 1
+  (3..n).each do |count|
+    x = sum
+    sum = sum_pre + sum
+    sum_pre = x
+  end
+  sum
+end
+
+def fibs_rec(n)
+  return 0 if n <= 0
+  return 1 if n <= 2
+  fibs_rec(n-1) + fibs_rec(n-2)
+end
+
+(1..20).each do |count|
+  puts "Num: #{count} : fibs: #{fibs(count)}"
+end
+
+(1..20).each do |count|
+  puts "Num: #{count} : fibs_rec: #{fibs_rec(count)}"
+end
